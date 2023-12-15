8c4bf2e3119492fbd3a1d1bc619b5d1b05d789dd
alroyhery <alroy2711@gmail.com>
2023-12-16 02:36:09 +0800

    commit

---
+++ application/views/barang_keluar/add.php
@@ -0,0 +1,91 @@
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card shadow-sm border-bottom-primary">
+            <div class="card-header bg-white py-3">
+                <div class="row">
+                    <div class="col">
+                        <h4 class="h5 align-middle m-0 font-weight-bold text-primary">
+                            Input Item Out Form
+                        </h4>
+                    </div>
+                    <div class="col-auto">
+                        <a href="<?= base_url('barangkeluar') ?>" class="btn btn-sm btn-secondary btn-icon-split">
+                            <span class="icon">
+                                <i class="fa fa-arrow-left"></i>
+                            </span>
+                            <span class="text">
+                                Back
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <?= $this->session->flashdata('pesan'); ?>
+                <?= form_open('', [], ['id_barang_keluar' => $id_barang_keluar, 'user_id' => $this->session->userdata('login_session')['user']]); ?>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="id_barang_keluar">Transaction Id Item Out</label>
+                    <div class="col-md-4">
+                        <input value="<?= $id_barang_keluar; ?>" type="text" readonly="readonly" class="form-control">
+                        <?= form_error('id_barang_keluar', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="tanggal_keluar">Date Out</label>
+                    <div class="col-md-4">
+                        <input value="<?= set_value('tanggal_keluar', date('Y-m-d')); ?>" name="tanggal_keluar" id="tanggal_keluar" type="text" class="form-control date" placeholder="Tanggal Masuk...">
+                        <?= form_error('tanggal_keluar', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="barang_id">Item</label>
+                    <div class="col-md-5">
+                        <div class="input-group">
+                            <select name="barang_id" id="barang_id" class="custom-select">
+                                <option value="" selected disabled>Choose Item</option>
+                                <?php foreach ($barang as $b) : ?>
+                                    <option value="<?= $b['id_barang'] ?>"><?= $b['id_barang'] . ' | ' . $b['nama_barang'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <div class="input-group-append">
+                                <a class="btn btn-primary" href="<?= base_url('barang/add'); ?>"><i class="fa fa-plus"></i></a>
+                            </div>
+                        </div>
+                        <?= form_error('barang_id', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="stok">Stock</label>
+                    <div class="col-md-5">
+                        <input readonly="readonly" id="stok" type="number" class="form-control">
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="jumlah_keluar">Total Out</label>
+                    <div class="col-md-5">
+                        <div class="input-group">
+                            <input value="<?= set_value('jumlah_keluar'); ?>" name="jumlah_keluar" id="jumlah_keluar" type="number" class="form-control" placeholder="Jumlah Keluar...">
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="satuan">Unit</span>
+                            </div>
+                        </div>
+                        <?= form_error('jumlah_keluar', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-4 text-md-right" for="total_stok">Total Stock</label>
+                    <div class="col-md-5">
+                        <input readonly="readonly" id="total_stok" type="number" class="form-control">
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col offset-md-4">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                    </div>
+                </div>
+                <?= form_close(); ?>
+            </div>
+        </div>
+    </div>
+</div>
